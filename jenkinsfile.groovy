8c955756545701c9fed86c8e1de7d3a63137edd6
ahridin <ahridin@gmail.com>
2016-12-14 12:45:08 +0200

    adding jenkinsfile

---
+++ jenkinsfile.groovy
@@ -0,0 +1,24 @@
+#!groovy
+stage('Build') {
+    /* .. snip .. */
+}
+
+stage('Test') {
+    parallel linux: {
+        node('linux') {
+            checkout scm
+            try {
+                unstash 'app'
+                sh 'make check'
+            }
+            finally {
+                junit '**/target/*.xml'
+            }
+        }
+    },
+    windows: {
+        node('windows') {
+            /* .. snip .. */
+        }
+    }
+}
