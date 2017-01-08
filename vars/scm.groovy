9f10e9003f2893487429f9b615ee4a013dab9d63
ahridin <ahridin@gmail.com>
2017-01-08 15:59:04 +0200

    test

--- vars/scm.groovy
+++ vars/scm.groovy
@@ -1,9 +1,25 @@
 #!groovy
-def credentials="credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397'";
+def scmType='SubversionSCM'
+def scmCredentialsId='29bae92d-6b9c-4f76-a54e-5b72f851a397'
+def scmRemote='https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman'
+def scmUpdater='UpdateUpdater'
 
 def checkout() {
 	println "CSM checkout!"
-    checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'UpdateUpdater']])
+    checkout([$class: scmType, additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: scmCredentialsId, depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: scmRemote]], workspaceUpdater: [$class: scmUpdater]])
 	}
-
 return this;
+
+
+
+ 
+
+
+
+////catchError {
+    // some block
+//}
+
+//ckean workspac when done step([$class: 'WsCleanup', deleteDirs: true, notFailBuild: true])
+//log parser step([$class: 'LogParserPublisher', showGraphs: true, useProjectRule: false])
+//figlet 'SVN checkout'
