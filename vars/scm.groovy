af354e8fb9fdd343b85b20b593b569fe4cf3a3f8
ahridin <ahridin@gmail.com>
2017-01-08 13:32:15 +0200

    checkout

--- vars/scm.groovy
+++ vars/scm.groovy
@@ -2,30 +2,8 @@
 def credentials="credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397'";
 
 def checkout() {
-  /*checkout([$class: 'GitSCM', 
-			branches: [[name: '*/master']], 
-			doGenerateSubmoduleConfigurations: false, 
-			extensions: [], 
-			gitTool: 'Default', 
-			submoduleCfg: [], 
-			userRemoteConfigs: [[credentialsId: '0a33916f-a110-4108-bb0a-e94ff08420fe', url: 'xxx']]
-		])*/
-  checkout([$class: 'SubversionSCM', 
-			additionalCredentials: [], 
-			excludedCommitMessages: '', 
-			excludedRegions: '', 
-			excludedRevprop: '', 
-			excludedUsers: '', 
-			filterChangelog: false, 
-			ignoreDirPropChanges: false, 
-			includedRegions: '', 
-			locations: [[
-				credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', 
-				depthOption: 'infinity', ignoreExternalsOption: false, 
-				local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman'
-			]], 
-			workspaceUpdater: [$class: 'UpdateUpdater']
-		])
-}
+	println "CSM checkout!"
+    checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'UpdateUpdater']])
+	}
 
 return this;
