e5584eef204e6123eb4edf8d31dd5807bde1bf8a
ahridin <ahridin@gmail.com>
2017-01-10 01:18:06 +0200

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,13 +1,10 @@
 #!groovy
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
+def call(body) { // evaluate the body block, and collect configuration into the object    
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-
-    // now build, based on the configuration provided
-    node {
+    node {  // now build, based on the configuration provided
         stage('checkout') {
                 println "===> CSM checkout ${config.environment}"
 	        checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'CheckoutUpdater']])
