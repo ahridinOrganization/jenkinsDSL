1157444806b9d5fa0b5167be79a768398c2b426a
ahridin <ahridin@gmail.com>
2017-01-09 15:36:06 +0200

    Update stage0.groovy

--- vars/stage0.groovy
+++ vars/stage0.groovy
@@ -4,10 +4,11 @@
  */
 def pipeline
 node {
+    helloworld.printHello("Hello from STAGE-0!")
     //pipeline = load 'pipeline.groovy'
     //pipeline.devQAStaging()
-    pipeline = load 'vars/helloworld.groovy'
-    helloworld.printHello("Hello from STAGE-0!")
+    //pipeline = load 'vars/helloworld.groovy'
+    //helloworld.printHello("Hello from STAGE-0!")
     
 }
 //pipeline.production()
