dee20211334b987b967897936ed98bb8cf59a297
ahridin <ahridin@gmail.com>
2017-01-09 17:23:56 +0200

    Update stage0.groovy

--- vars/stage0.groovy
+++ vars/stage0.groovy
@@ -3,14 +3,14 @@
  * Prints a stub message for testing purposes.
  */
 def helloworld = fileLoader.load('vars/helloworld'); 
-def call () {
+//def call() {
     node {
-        echo ("Hello I am stage0 groovy and I am calling helloworld")
+    //    echo ("Hello I am stage0 groovy and I am calling helloworld")
         helloworld.printHello("Hello from STAGE-00000!")
-        checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: scmRemote]], workspaceUpdater: [$class: scmUpdater]])
-    }
+      //  checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: scmRemote]], workspaceUpdater: [$class: scmUpdater]])
+   }
     
-}
+//}
 //pipeline.production()
 
 return this;
