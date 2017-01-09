f03e5046e19607b593e39ea69bfbca5520ede06d
ahridin <ahridin@gmail.com>
2017-01-09 14:47:10 +0200

    Create stage0.groovy

---
+++ vars/stage0.groovy
@@ -0,0 +1,13 @@
+#!groovy
+/**
+ * Prints a stub message for testing purposes.
+ */
+
+def version = '1.0'
+
+def printHello(String msg) {
+  //println "Hello, world!"
+  println msg;
+}
+
+return this;
