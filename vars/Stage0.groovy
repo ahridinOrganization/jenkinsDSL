64372bfbdf2eaaee3021f8c6808bea030c09614f
ahridin <ahridin@gmail.com>
2017-01-09 13:28:17 +0200

    Update Stage0.groovy

--- vars/Stage0.groovy
+++ vars/Stage0.groovy
@@ -1,1 +1,12 @@
 
+job('stage-0') {
+    scm {
+        git('git://github.com/quidryan/aws-sdk-test.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
