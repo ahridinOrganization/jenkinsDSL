ffc72ed7794e9137afd184af3e03b0baf73302f2
ahridin <ahridin@gmail.com>
2017-01-09 17:14:53 +0200

    Update stage0.groovy

--- vars/stage0.groovy
+++ vars/stage0.groovy
@@ -5,7 +5,7 @@
 def helloworld = fileLoader.load('vars/helloworld'); 
 def call () {
     node {
-        
+        echo ("Hello I am stage0 groovy and I am calling helloworld")
         helloworld.printHello("Hello from STAGE-00000!")
         checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: scmRemote]], workspaceUpdater: [$class: scmUpdater]])
     }
