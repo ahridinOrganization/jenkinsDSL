cb05d7486d23b27c31ddb7cd4c7d9dfd84338cfc
ahridin <ahridin@gmail.com>
2017-01-09 14:14:37 +0200

    Update Stage0.groovy

--- vars/Stage0.groovy
+++ vars/Stage0.groovy
@@ -1,9 +1,21 @@
 
-def helloworld = fileLoader.load('helloworld');
-def environment = fileLoader.load('environment');
- //the you can call the function in file1 as
-
-stage ('Preparations') {
+import com.cloudbees.groovy.cps.NonCPS
+def version = '1.0'
+@NonCPS
+def run() {
+  def helloworld = fileLoader.load('helloworld');
+  def environment = fileLoader.load('environment');
+  
+  stage ('Preparations') {
 	helloworld.printHello("Good morning!")
 	environment.dumpEnvVars()			
+	}
+	
 }
+return this;
+
+
+
+ //the you can call the function in file1 as
+
+
