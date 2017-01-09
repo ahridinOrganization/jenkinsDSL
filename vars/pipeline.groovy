103de7f32d2dc6a2d8e9f12f9d2598f22c1f66ab
ahridin <ahridin@gmail.com>
2017-01-10 01:16:07 +0200

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -9,8 +9,7 @@ def call(body) {
     // now build, based on the configuration provided
     node {
         stage('checkout') {
-            println "===> CSM checkout $scmRemote"
-            println "===> CSM checkout ${config.environment}"
+                println "===> CSM checkout ${config.environment}"
 	        checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'CheckoutUpdater']])
 	       //git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
         }
