1c2bab232bf1542eceed2e6274a8ab6c9f267115
ahridin <ahridin@gmail.com>
2017-01-10 00:30:31 +0200

    test

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,3 +1,7 @@
+def scmCredentialsId='29bae92d-6b9c-4f76-a54e-5b72f851a397'
+def scmRemote='https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman'
+def scmUpdater='CheckoutUpdater'
+
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
@@ -7,8 +11,34 @@ def call(body) {
 
     // now build, based on the configuration provided
     node {
-        git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
-        sh "mvn install"
-        mail to: "...", subject: "${config.name} plugin build", body: "..."
+        stage('checkout') {
+            println "===> CSM checkout $scmRemote"
+            println "===> CSM checkout ${config.environment}"
+	        checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: scmRemote]], workspaceUpdater: [$class: scmUpdater]])
+	       //git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
+        }
+        stage ('build'){
+            //sh "mvn install"
+            // mail to: "...", subject: "${config.name} plugin build", body: "..."
+        }
     }
 }
+/*
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    node {
+        stage('Checkout') {
+            checkout scm
+        }
+        stage('Main') {
+            docker.image(config.environment).inside {
+                sh config.mainScript
+            }
+        }
+        stage('Post') {
+            sh config.postScript
+        }
+    }*/
