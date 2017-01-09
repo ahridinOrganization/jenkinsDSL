76acf4056fd412410b5e8aeb06c47912697f7c8c
ahridin <ahridin@gmail.com>
2017-01-09 11:13:35 +0200

    Update scm.groovy

--- vars/scm.groovy
+++ vars/scm.groovy
@@ -1,9 +1,9 @@
 #!groovy
      def scmCredentialsId='29bae92d-6b9c-4f76-a54e-5b72f851a397'
-     def scmRemote='https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman'
+     //def scmRemote='https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman'
      def scmUpdater='UpdateUpdater'
-def checkout() {
-	println "========= CSM checkout ========= "
+def checkout(String scmRemote) {
+	println "===> CSM checkout"
     	checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: scmCredentialsId, depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: scmRemote]], workspaceUpdater: [$class: scmUpdater]])
 	}
 
