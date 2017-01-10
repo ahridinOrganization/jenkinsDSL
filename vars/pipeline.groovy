1793d8d803cf9c71442a3e689597eca4211d05bb
ahridin <ahridin@gmail.com>
2017-01-10 02:27:48 +0200

    jjj

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,18 +1,21 @@
-#!groovy
-def call(body) { // evaluate the body block, and collect configuration into the object    
+def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    node {  // now build, based on the configuration provided
-        stage('checkout') {
-                println "===> CSM checkout ${config.environment}"
-	        checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'CheckoutUpdater']])
-	       //git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
+    node {
+        stage('Checkout') {
+            //checkout scm
+            checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'CheckoutUpdater']])
+
         }
-        stage ('build'){
-            //sh "mvn install"
-            // mail to: "...", subject: "${config.name} plugin build", body: "..."
+        stage('Main') {
+            docker.image(config.environment).inside {
+                sh config.mainScript
+            }
+        }
+        stage('Post') {
+            sh config.postScript
         }
     }
 }
