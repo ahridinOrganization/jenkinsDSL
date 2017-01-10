b2269492b3620ab75b76890a21ff66fb5ffc8859
ahridin <ahridin@gmail.com>
2017-01-10 02:32:16 +0200

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -19,22 +19,3 @@ def call(body) {
         }
     }
 }
-/*
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    node {
-        stage('Checkout') {
-            checkout scm
-        }
-        stage('Main') {
-            docker.image(config.environment).inside {
-                sh config.mainScript
-            }
-        }
-        stage('Post') {
-            sh config.postScript
-        }
-    }*/
