cb1f130e544b4782061f099ef488280b637c5968
ahridin <ahridin@gmail.com>
2017-01-10 03:24:01 +0200

    Update stage_0.groovy

--- vars/stage_0.groovy
+++ vars/stage_0.groovy
@@ -6,7 +6,7 @@ def call(body) {
     println (nodeNames().join(",").toString())
     node (){
          stage("Checkout") {
-            checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'CheckoutUpdater']])        
+            checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: '${config.repoUrl}']], workspaceUpdater: [$class: 'CheckoutUpdater']])        
          }
         stage('Main') {
             docker.image(config.environment).inside { sh config.mainScript}
