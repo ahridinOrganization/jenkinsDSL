c6eb02940200bf328b3538bbecf846448a4eae34
ahridin <ahridin@gmail.com>
2017-01-10 03:20:42 +0200

    Update stage_0.groovy

--- vars/stage_0.groovy
+++ vars/stage_0.groovy
@@ -3,7 +3,7 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    orintln (nodeNames().join(",").toString())
+    println (nodeNames().join(",").toString())
     node (){
          stage("Checkout") {
             checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'CheckoutUpdater']])        
