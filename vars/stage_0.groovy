def5540b829fdc9897abb528daca8b42751440e8
ahridin <ahridin@gmail.com>
2017-01-10 03:08:09 +0200

    Update stage_0.groovy

--- vars/stage_0.groovy
+++ vars/stage_0.groovy
@@ -3,16 +3,25 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    node {
-        wrap([$class: 'AnsiColorBuildWrapper']) {
-        stage("\u001B[31mI'm Red\u001B[0m Checkout") {
-            checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'CheckoutUpdater']])
-
-        }
+    def slave = nodeNames()
+    node (slave){
+         build job: 'test_jobs', parameters: [[$class: 'StringParameterValue', name: 'param1', value:
+            'test_param'], [$class: 'StringParameterValue', name:'dummy', value: "${index}"]]
+         }
+         stage("Checkout") {
+            checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'CheckoutUpdater']])        
+         }
         stage('Main') {
             docker.image(config.environment).inside { sh config.mainScript}
         }
         stage('Post') { sh config.postScript}
         }
-    }
 }
+
+// This method collects a list of Node names from the current Jenkins instance
+@NonCPS
+def nodeNames() {
+  return jenkins.model.Jenkins.instance.nodes.collect { node -> (node.name=="linux")? node:null}
+}
+
+
