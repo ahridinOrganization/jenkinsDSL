b404d0001a2ce4de700788a5668cfa4518be391b
ahridin <ahridin@gmail.com>
2017-01-11 23:43:13 +0200

    Update BuildFramework.groovy

--- vars/BuildFramework.groovy
+++ vars/BuildFramework.groovy
@@ -1,9 +1,9 @@
 class BuildFramework {
+    
+    println (jobName)
+    println (antTargets)
     static ant(dslFactory, jobName, antTargets) {
         dslFactory.freeStyleJob(jobName) {
-            steps {
-                ant(antTargets)
-            }
-        }
+              }
     }
 }
