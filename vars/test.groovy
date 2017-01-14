b6e83d1dceca3067550b142b0893100166993622
ahridin <ahridin@gmail.com>
2017-01-15 01:52:59 +0200

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,19 @@
+import utilities.Defaults
+
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
     node {
-        def job=stage0(MESSAGE:config.MESSAGE)
+    //    def job=stage0(MESSAGE:config.MESSAGE)
         //jobDsl scriptText:
-    }
+  
+ 
+    Defaults.getBaseJob(freeStyleJob('myJob')){
+    scm {
+      git ('git@github.com/william/the-juggler.git')
+   }
+          }
+}
 }
