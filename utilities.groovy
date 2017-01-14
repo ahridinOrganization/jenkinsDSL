491c5d4d7dce485b92f9ad57bf7d2868caffb80a
ahridin <ahridin@gmail.com>
2017-01-15 01:49:39 +0200

    Create utilities.groovy

---
+++ utilities.groovy
@@ -0,0 +1,14 @@
+#!groovy
+package utilities
+
+public class Defaults {
+   static def getBaseJob(def job, Closure optionalClosure = null){
+      job.with {
+         description 'A default description.'
+      }
+      if(optionalClosure) {
+         optionalClosure.delegate = job
+         optionalClosure.run()
+      }
+   }
+}
