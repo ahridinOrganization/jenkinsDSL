56f74b4d1eec26740c5afe998f186315f2fa83d4
ahridin <ahridin@gmail.com>
2017-01-14 20:30:20 +0200

    Update stage0.groovy

--- jobs/stage0.groovy
+++ jobs/stage0.groovy
@@ -1,52 +1,13 @@
-String basePath = 'example9'
-String repo = ':pserver:anoncvs@example.org:/cvsroot'
+String basePath = 'example'
 
 folder(basePath) {
     description 'This example shows hwp to use automatically generated DSL provided by other plugins.'
 }
 
-job("$basePath/grails-legacy-build") {
-    scm {
-        cvsscm {
-            repositories {
-                cvsRepository {
-                    cvsRoot repo
-                    passwordRequired false
-                    password null
-                    compressionLevel(-1)
-                    repositoryBrowser {}
-                    repositoryItems {
-                        cvsRepositoryItem {
-                            modules {
-                                cvsModule {
-                                    remoteName 'sheehan/grails-example'
-                                    localName '.'
-                                    projectsetFileName null
-                                }
-                            }
-                            location {
-                                headRepositoryLocation()
-                            }
-                        }
-                    }
-                }
-            }
-            canUseUpdate true
-            pruneEmptyDirectories true
-            legacy false
-            skipChangeLog false
-            disableCvsQuiet false
-            cleanOnFailedUpdate false
-            forceCleanCopy false
-        }
-    }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    steps {
-        grails {
-            useWrapper true
-            targets(['test-app', 'war'])
-        }
-    }
+freeStyleJob("$basePath/${config.MESSAGE}") {
+   parameters {
+     stringParam(MESSAGE, ${config.MESSAGE}) 
+   }
+   steps {   bat('echo $MESSAGE')  }
 }
+
