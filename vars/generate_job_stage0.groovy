017dbd49c06bf9efac3588d311e2e1ffa74e479e
ahridin <ahridin@gmail.com>
2017-01-15 18:39:25 +0200

    Update generate_job_stage0.groovy

--- vars/generate_job_stage0.groovy
+++ vars/generate_job_stage0.groovy
@@ -50,9 +50,10 @@ def call(body) {
                             fallbackScript('return ["jdk6_32bit", "jdk7_32bit","jdk7_64bit","jdk8_64bit"]')
                             }   
                     } */
-                    choiceParam('JDK_VERISON', ["${config.JDK_VERSION}", "jdk7_64bit","jdk7_32bit","jdk8_64bit","jdk6_32bit"], 'JDK')
+                    //choiceParam('JDK_VERISON', ["${config.JDK_VERSION}", "jdk7_64bit","jdk7_32bit","jdk8_64bit","jdk6_32bit"], 'JDK')
                     //booleanParam('RUN_TESTS', true, 'uncheck to disable tests')
-                    stringParam("MVN_POM", "${config.MVN_POM}","Root POM name")
+		    stringParam("JDK_VERISON", "${config.JDK_VERISON}","JDK Version")
+		    stringParam("MVN_POM", "${config.MVN_POM}","Root POM name")
                     stringParam("MVN_GOALS", "${config.MVN_GOALS}","Maven goals to execute")
                     stringParam("TAG_URL", "${config.TAG_URL}","Full SVN URL to tags (without tag version)")                        
                     /*listTagsParam('TAG_URL',"${config.TAG_URL}") {
