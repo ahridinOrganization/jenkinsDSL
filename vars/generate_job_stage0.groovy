33b2c5d1ea42cede0d5e2e2bd402c14440f8e2e3
ahridin <ahridin@gmail.com>
2017-01-15 10:59:18 +0200

    Update generate_job_stage0.groovy

--- vars/generate_job_stage0.groovy
+++ vars/generate_job_stage0.groovy
@@ -94,8 +94,8 @@ def call(body) {
                         //localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
                         localRepository(LocalRepositoryLocation.LOCAL_TO_EXECUTOR)
                         //properties(skipTests: true)
-                        mavenName("${config.MVN_VERSION}")                        
-                        mavenInstallation("${config.MVN_VERSION}")
+                                              
+                        mavenInstallation('Maven 3.0.4')
                         injectBuildVariables(true)
                         rootPOM('\${MVN_POM}')
                         //providedSettings('central-mirror')
