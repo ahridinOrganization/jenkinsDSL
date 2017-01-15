2f2c20346c56ea4a2e93d35c3b58ef922dca89ae
ahridin <ahridin@gmail.com>
2017-01-15 09:31:51 +0200

    Update generate_job_stage0.groovy

--- vars/generate_job_stage0.groovy
+++ vars/generate_job_stage0.groovy
@@ -88,7 +88,7 @@ def call(body) {
                     //systemGroovyCommand(readFileFromWorkspace('disconnect-slave.groovy')) {binding('computerName', 'ubuntu-04') }
                     //systemGroovyCommand { println("JDK_VERISON = '\${JDK_VERISON}'") binding('computerName', 'ubuntu-04') }                                                          
                     maven {
-                        goals('\${MVN_GOALS} --nobanner --exception --debug --show-version --info') 
+                        goals('\${MVN_GOALS} -B -X -V') 
                         mavenOpts('-XX:MaxPermSize=128m -Xmx768m')
                         localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
                         //properties(skipTests: true)
