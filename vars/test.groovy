4211877c781c464de3a65961b534d5ea6171ee29
ahridin <ahridin@gmail.com>
2017-01-15 19:08:15 +0200

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -13,7 +13,8 @@ def call(body) {
     node {
     //    def job=stage0(MESSAGE:config.MESSAGE)
         //jobDsl scriptText:
-   jobDsl lookupStrategy: 'SEED_JOB', removedJobAction: 'DISABLE', removedViewAction: 'DELETE', scriptText: 'stage0.groovy', unstableOnDeprecation: true
+   jobDsl ignoreMissingFiles: true, lookupStrategy: 'SEED_JOB', removedJobAction: 'DISABLE', removedViewAction: 'DELETE', targets: 'stage0.groovy', unstableOnDeprecation: true
+   //jobDsl lookupStrategy: 'SEED_JOB', removedJobAction: 'DISABLE', removedViewAction: 'DELETE', scriptText: 'stage0.groovy', unstableOnDeprecation: true
    /*jobDsl targets: ['stage0.groovy'].join('\n'),
            removedJobAction: 'DELETE',
            removedViewAction: 'DELETE',
