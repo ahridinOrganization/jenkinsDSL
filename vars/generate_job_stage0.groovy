c4a8efcc1ef62938765f3cfe7b25323240c34536
ahridin <ahridin@gmail.com>
2017-01-15 18:27:03 +0200

    Update generate_job_stage0.groovy

--- vars/generate_job_stage0.groovy
+++ vars/generate_job_stage0.groovy
@@ -101,6 +101,12 @@ def call(body) {
                         rootPOM('\${MVN_POM}')
                         //providedSettings('central-mirror')
                         } 
+		    maven {
+			 goals('build-helper:parse-version -B -X -V')
+			 goals('versions:set -B -X -V')
+			 goals('-DnewVersion=$NEW_POM_VERSION scm:checkin -Dmessage="build version from jenkins job" -DpushChanges -B -X -V')
+			 mavenInstallation("${config.MVN_VERSION}")                       
+                        }	
                                  
                 } //end steps 
             } //end freeStyleJob        
