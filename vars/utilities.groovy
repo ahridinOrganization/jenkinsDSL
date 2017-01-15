775dfcd3d829865f21378114dc6ec4b11163be65
ahridin <ahridin@gmail.com>
2017-01-15 02:29:29 +0200

    Update utilities.groovy

--- vars/utilities.groovy
+++ vars/utilities.groovy
@@ -1,14 +1,16 @@
 #!groovy
+
+
+
 //package utilities
-@NonCPS method
+
 //public class Defaults {
-   static def getBaseJob(def job, Closure optionalClosure = null){
-      job.with {
-         description 'A default description.'
-      }
+  static def getBaseJob(def job, Closure optionalClosure = null){
+      job.with { description 'A default description.' }
       if(optionalClosure) {
          optionalClosure.delegate = job
          optionalClosure.run()
       }
    }
+return this;
 //}
