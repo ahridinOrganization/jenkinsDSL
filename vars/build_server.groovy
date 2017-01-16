c09bdf4cdeac77bc77ebf402353c68a7879698e4
ahridin <ahridin@gmail.com>
2017-01-16 02:54:54 +0200

    Update build_server.groovy

--- vars/build_server.groovy
+++ vars/build_server.groovy
@@ -49,10 +49,10 @@ def call(body) {
 		    stringParam("ARTIFACTS", "${config.ARTIFACTS_REGEX}","Artifacts (regex)")                        
 		    labelParam("SLAVE_LABEL',"${config.SLAVE_LABEL}")
 		    booleanParam('SKIP_TESTS', false, "check to disable tests")
-                    /*listTagsParam('TAG_URL',"${config.TAG_URL}") {
+                    listTagsParam('TAG_URL',"${config.TAG_URL}") {
                         credentialsId('c2b9fdc3-7562-4bc4-b4f6-3de05444999e')                                                
                         sortNewestFirst()
-                    }*/                               
+                    }                               
 		    booleanParam('CLEANUP', true, 'uncheck to disable workspace cleanup')
                 } //end parameters
                  // ====================== PROPERTIES =============================
