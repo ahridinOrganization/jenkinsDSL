7f300ad15db8b88e67343fb883727aafb1c896d1
ahridin <ahridin@gmail.com>
2017-01-16 17:48:44 +0200

    Update test2.groovy

--- vars/test2.groovy
+++ vars/test2.groovy
@@ -5,9 +5,22 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    jobDsl targets: "mavenJob.groovy",
+    /*jobDsl targets: "mavenJob.groovy",
            removedJobAction: 'DELETE',
            removedViewAction: 'DELETE',
            lookupStrategy: 'SEED_JOB',           
-
+*/
+ public static def job = { dslFactory, jobName ->
+        return dslFactory.job(${jobName}") {
+                 parameters {
+                stringParam('CONFIGURATION_REPO', extraVars.get('CONFIGURATION_REPO', 'https://github.com/edx/configuration.git'),
+                            'Git repo containing the analytics pipeline configuration automation.')
+                stringParam('CONFIGURATION_BRANCH', extraVars.get('CONFIGURATION_BRANCH', 'master'),
+                            'e.g. tagname or origin/branchname')
+            }
+                              
+            /*
+               Run arbitrary remote commands on a host belonging to a target environment, deployment and cluster,
+               in a specified region.
+             */
 }
