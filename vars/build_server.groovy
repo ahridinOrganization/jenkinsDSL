410aae6393c6e1a201ac16470fbbe5b66cc2de77
ahridin <ahridin@gmail.com>
2017-01-16 02:59:35 +0200

    Update build_server.groovy

--- vars/build_server.groovy
+++ vars/build_server.groovy
@@ -8,7 +8,7 @@ def call(body) {
         jobDsl scriptText:"""
             folder("${jobFolder}")
             freeStyleJob("${jobFolder}/${config.NAME}") {
-		description("Auto generated ${config.NAME} stage-0 job")
+		description("Auto generated ${config.NAME} build job")
                 logRotator(21,-1,-1,-1) //(daysToKeep,numToKeep,artifactDaysToKeep,artifactNumToKeep)
                 concurrentBuild()
                 quietPeriod(2) 
@@ -47,7 +47,7 @@ def call(body) {
                     stringParam("MVN_GOALS", "${config.MVN_GOALS}","Maven goals to execute")
                     stringParam("TAG_URL", "${config.TAG_URL}","Full SVN URL to tags (without tag version)")                        
 		    stringParam("ARTIFACTS", "${config.ARTIFACTS_REGEX}","Artifacts (regex)")                        
-		    labelParam("SLAVE_LABEL',"${config.SLAVE_LABEL}")
+		    labelParam("SLAVE_LABEL", "${config.SLAVE_LABEL}","")
 		    booleanParam('SKIP_TESTS', false, "check to disable tests")
                     listTagsParam('TAG_URL',"${config.TAG_URL}") {
                         credentialsId('c2b9fdc3-7562-4bc4-b4f6-3de05444999e')                                                
