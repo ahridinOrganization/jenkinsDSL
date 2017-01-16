69ceeba88323f8d54a5da9c5455f67c67e646940
ahridin <ahridin@gmail.com>
2017-01-16 18:07:38 +0200

    Update test2.groovy

--- vars/test2.groovy
+++ vars/test2.groovy
@@ -5,12 +5,22 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+    
+    
+    jobDsl targets: ['mavenJob.groovy'].join('\n'),
+           removedJobAction: 'DISABLE',
+           removedViewAction: 'DELETE',
+           lookupStrategy: 'SEED_JOB',
+           additionalClasspath: ['libA.jar', 'libB.jar'].join('\n')
+    
+    
     /*jobDsl targets: "mavenJob.groovy",
            removedJobAction: 'DELETE',
            removedViewAction: 'DELETE',
            lookupStrategy: 'SEED_JOB',           
 */
  public static def job = { 
+     
      dslFactory, jobName -> return dslFactory.job("MyJob") {
           parameters {
             stringParam('CONFIGURATION_REPO', 'https://github.com/edx/configuration.git')                      
