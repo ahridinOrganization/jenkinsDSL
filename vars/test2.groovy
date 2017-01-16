e965e9768c0ea04b57056b1a85ccb73e62989b15
ahridin <ahridin@gmail.com>
2017-01-16 16:22:51 +0200

    Update test2.groovy

--- vars/test2.groovy
+++ vars/test2.groovy
@@ -6,13 +6,11 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    
-    buildFlowJob(String name, Closure closure = null)
     println "${config.Name}"
     node () {    
         freeStyleJob("${jobFolder}/${config.Name}")
         git url: 'https://github.com/jfrogdev/project-examples.git'
-        
+        buildFlowJob(String name, Closure closure = null)
         
         }
     
