48ba566bbcffb902f84ff4bda645f6164e8151db
ahridin <ahridin@gmail.com>
2017-01-16 18:48:21 +0200

    Update test2.groovy

--- vars/test2.groovy
+++ vars/test2.groovy
@@ -5,11 +5,11 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    
+    def job
     node {
         //def job1 = makeMeABasicJob(this) //Passing the groovy file class as the resolution context
         //job1.with({ steps {  shell('echo Hello') }  })
-        BuildFramework.ant(this, 'my-ant-project', 'clean build')
+        BuildFramework.ant(job, 'my-ant-project', 'clean build')
     }
 }
 
