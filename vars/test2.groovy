c98d1b026cb00edd398882d818cc3e80b2f32c47
ahridin <ahridin@gmail.com>
2017-01-16 17:56:41 +0200

    Update test2.groovy

--- vars/test2.groovy
+++ vars/test2.groovy
@@ -10,17 +10,15 @@ def call(body) {
            removedViewAction: 'DELETE',
            lookupStrategy: 'SEED_JOB',           
 */
- public static def job = { dslFactory, jobName ->
-        return dslFactory.job("${config.name}") {
-                 parameters {
-                stringParam('CONFIGURATION_REPO', extraVars.get('CONFIGURATION_REPO', 'https://github.com/edx/configuration.git'),
-                            'Git repo containing the analytics pipeline configuration automation.')
-                stringParam('CONFIGURATION_BRANCH', extraVars.get('CONFIGURATION_BRANCH', 'master'),
-                            'e.g. tagname or origin/branchname')
+ public static def job = { 
+     dslFactory, jobName -> return dslFactory.job("MyJob") {
+          parameters {
+            stringParam('CONFIGURATION_REPO', 'https://github.com/edx/configuration.git')                      
+            stringParam('CONFIGURATION_BRANCH', 'master')                      
                  }
-        }
+           }
                               
- }     /*
+     }     /*
                Run arbitrary remote commands on a host belonging to a target environment, deployment and cluster,
                in a specified region.
              */
