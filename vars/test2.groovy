4f26a76bd6bc69812a201fd361eb898c4c9b4f56
ahridin <ahridin@gmail.com>
2017-01-16 15:49:49 +0200

    Rename test.groovy to test2.groovy

---
+++ vars/test2.groovy
@@ -0,0 +1,10 @@
+def call(body) {
+    def config = [:]
+    def jobFolder="STAGE-0"
+    def job	
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
+    println ${config.Name}
+ }
