d2869f9ed8194ca21c1330dc3f6479a86136851d
ahridin <ahridin@gmail.com>
2017-01-16 18:46:10 +0200

    Update test2.groovy

--- vars/test2.groovy
+++ vars/test2.groovy
@@ -7,8 +7,8 @@ def call(body) {
     body()
     
     node {
-        def job1 = makeMeABasicJob(this) //Passing the groovy file class as the resolution context
-        job1.with({ steps {  shell('echo Hello') }  })
+        //def job1 = makeMeABasicJob(this) //Passing the groovy file class as the resolution context
+        //job1.with({ steps {  shell('echo Hello') }  })
         BuildFramework.ant(this, 'my-ant-project', 'clean build')
     }
 }
