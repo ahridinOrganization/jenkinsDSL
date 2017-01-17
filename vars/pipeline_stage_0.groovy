260f490f3077b004a53ff5b64692b70bf8f34b80
ahridin <ahridin@gmail.com>
2017-01-18 01:12:07 +0200

    Update pipeline_stage_0.groovy

--- vars/pipeline_stage_0.groovy
+++ vars/pipeline_stage_0.groovy
@@ -7,39 +7,7 @@ def call(body) {
     body()
     node {       
         def workspace = pwd() 
-         load "${workspace}@libs/github.com/ahridinOrganization/jenkinsDSL/vars/pipeline.groovy" 
-        
-    }
-pipeline {
-    // Make sure that the tools we need are installed and on the path.
-    tools {
-        maven "Maven 3.0.4"
-        jdk "Oracle JDK 8u40"
+         load "${workspace}@libs/github.com/ahridinOrganization/jenkinsDSL/vars/pipelineScript.groovy"           
     }
 
-    // Run on any executor.
-    agent any
-
-    // The order that sections are specified doesn't matter - this will still be run
-    // after the stages, even though it's specified before the stages.
-    post {
-        // No matter what the build status is, run these steps. There are other conditions
-        // available as well, such as "success", "failed", "unstable", and "changed".
-        always {
-            archive "target/**/*"
-            junit 'target/surefire-reports/*.xml'
-        }
-    }
-
-    stages {
-        // While there's only one stage here, you can specify as many stages as you like!
-        stage("build") {
-            steps {
-                echo "hello from stage-0"
-                //bat 'mvn clean install -Dmaven.test.failure.ignore=true'
-            }
-        }
-    }
-
-}
 }
