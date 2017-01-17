34aa65f9fe7916f672d4bcb24c8ecd4f216856e9
ahridin <ahridin@gmail.com>
2017-01-18 01:26:20 +0200

    Update pipeline_stage_0.groovy

--- vars/pipeline_stage_0.groovy
+++ vars/pipeline_stage_0.groovy
@@ -9,7 +9,8 @@ def call(body) {
         //freeStyleJob("${jobFolder}/${config.NAME}") {
         def workspace = pwd() 
         echo "Hello from pipeline_stage_0.groovy" 
-        load "${workspace}@libs/github.com/ahridinOrganization/jenkinsDSL/vars/pipelineScript.groovy"           
+        pipeline0
+        load "${workspace}@libs/github.com/ahridinOrganization/jenkinsDSL/vars/pipeline0.groovy"           
         //pipelineScript(config)
         pipeline0
     }
