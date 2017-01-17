03e8357a08e77019448ad78fe1adc046127bfb09
ahridin <ahridin@gmail.com>
2017-01-18 01:25:22 +0200

    Update pipeline_stage_0.groovy

--- vars/pipeline_stage_0.groovy
+++ vars/pipeline_stage_0.groovy
@@ -6,12 +6,12 @@ def call(body) {
     body.delegate = config
     body()
     node {       
-        mavenJob()
         //freeStyleJob("${jobFolder}/${config.NAME}") {
         def workspace = pwd() 
         echo "Hello from pipeline_stage_0.groovy" 
         load "${workspace}@libs/github.com/ahridinOrganization/jenkinsDSL/vars/pipelineScript.groovy"           
-        pipelineScript(config)
+        //pipelineScript(config)
+        pipeline0
     }
 
 }
