054f9aaff74bc074f3bba8dc859666a51ec94a27
ahridin <ahridin@gmail.com>
2017-01-18 03:52:01 +0200

    Update pipeline0.groovy

--- vars/pipeline0.groovy
+++ vars/pipeline0.groovy
@@ -1,4 +1,4 @@
-/*def call(body) {
+def call(body) {
     
     def config = [:]
     def jobFolder="STAGE-0"
@@ -7,38 +7,38 @@
     body.delegate = config
     body()
     
-    node {*/
-        pipeline{
+    node {
+        pipeline {
     // Make sure that the tools we need are installed and on the path.
         tools {
         maven "Maven 3.0.4"
         jdk "Oracle JDK 8u40"
-    }
-
-    // Run on any executor.
-    agent any
-
-    // The order that sections are specified doesn't matter - this will still be run
-    // after the stages, even though it's specified before the stages.
-    post {
-        // No matter what the build status is, run these steps. There are other conditions
-        // available as well, such as "success", "failed", "unstable", and "changed".
-        always {
-            archive "target/**/*"
-            junit 'target/surefire-reports/*.xml'
         }
-    }
 
-    stages {
-        // While there's only one stage here, you can specify as many stages as you like!
-        stage("build") {
-            steps {
-                echo """hello from stage-0 ${config.jobName}"""
-                //bat 'mvn clean install -Dmaven.test.failure.ignore=true'
+        // Run on any executor.
+        agent any
+
+        // The order that sections are specified doesn't matter - this will still be run
+        // after the stages, even though it's specified before the stages.
+        post {
+            // No matter what the build status is, run these steps. There are other conditions
+            // available as well, such as "success", "failed", "unstable", and "changed".
+            always {
+                archive "target/**/*"
+                junit 'target/surefire-reports/*.xml'
             }
         }
-    }
+
+        stages {
+            // While there's only one stage here, you can specify as many stages as you like!
+            stage("build") {
+                steps {
+                    echo """hello from stage-0 ${config.jobName}"""
+                    //bat 'mvn clean install -Dmaven.test.failure.ignore=true'
+                }
+            }
+        }
 
    }
-//    }
-//}
+   }
+}
