8fcb880592e0190ce80f47ff80aeea3816d600b0
ahridin <ahridin@gmail.com>
2017-01-18 08:19:27 +0200

    Update pipeline0.groovy

--- vars/pipeline0.groovy
+++ vars/pipeline0.groovy
@@ -6,84 +6,31 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    println "pipeline0. body"
     node {
         pipelineJob('pipelineJob') {
-    definition {
-        cps {
-            script('''
-pipeline {
-    // Make sure that the tools we need are installed and on the path.
-    tools {
-        maven "Maven 3.0.4"
-        jdk "Oracle JDK 8u40"
-    }
+            definition {
+                cps {
+                    script('''
+                            pipeline {
+                                tools { // Make sure that the tools we need are installed and on the path.
+                                    maven "Maven 3.0.4"
+                                    jdk "Oracle JDK 8u40"
+                                }
+                                
+                                agent any // Run on any executor.
+                                stages {
+                                    stage("build") {
+                                        steps {
+                                            echo "hello from stage-0 ${config.Name}"                                                                                        
+                                        }
+                                    }
+                                }
 
-    // Run on any executor.
-    agent any
-
-    // The order that sections are specified doesn't matter - this will still be run
-    // after the stages, even though it's specified before the stages.
-    //post {
-        // No matter what the build status is, run these steps. There are other conditions
-        // available as well, such as "success", "failed", "unstable", and "changed".
-      //  always {
-        //    archive "target/**/*"
-         //   junit 'target/surefire-reports/*.xml'
-        //}
-    //}
-
-    stages {
-        // While there's only one stage here, you can specify as many stages as you like!
-        stage("build") {
-            steps {
-                echo "hello from stage-0"
-                workflowJob("MyFlow") 
-                //bat 'mvn clean install -Dmaven.test.failure.ignore=true'
-            }
-        }
-    }
-
-}
-'''
-)
-            sandbox()
-        }
-    }
-}
-        
-        
-        pipeline {
-    // Make sure that the tools we need are installed and on the path.
-        tools {
-        maven "Maven 3.0.4"
-        jdk "Oracle JDK 8u40"
-        }
-
-        // Run on any executor.
-        agent any
-
-        // The order that sections are specified doesn't matter - this will still be run
-        // after the stages, even though it's specified before the stages.
-        post {
-            // No matter what the build status is, run these steps. There are other conditions
-            // available as well, such as "success", "failed", "unstable", and "changed".
-            always {
-                archive "target/**/*"
-                junit 'target/surefire-reports/*.xml'
-            }
-        }
-
-        stages {
-            // While there's only one stage here, you can specify as many stages as you like!
-            stage("build") {
-                steps {
-                    echo """hello from stage-0 ${config.jobName}"""
-                    //bat 'mvn clean install -Dmaven.test.failure.ignore=true'
+                            }
+                            ''')
+                    sandbox()
+                    }
                 }
             }
-        }
-
-   }
-   }
+        }        
 }
