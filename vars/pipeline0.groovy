3b8c2c9af0195f4b0502a08ca422499e0fecfc86
ahridin <ahridin@gmail.com>
2017-01-18 06:51:10 +0200

    Update pipeline0.groovy

--- vars/pipeline0.groovy
+++ vars/pipeline0.groovy
@@ -8,6 +8,51 @@ def call(body) {
     body()
     println "pipeline0. body"
     node {
+        pipelineJob('pipelineJob') {
+    definition {
+        cps {
+            script('''
+pipeline {
+    // Make sure that the tools we need are installed and on the path.
+    tools {
+        maven "Maven 3.0.4"
+        jdk "Oracle JDK 8u40"
+    }
+
+    // Run on any executor.
+    agent any
+
+    // The order that sections are specified doesn't matter - this will still be run
+    // after the stages, even though it's specified before the stages.
+    //post {
+        // No matter what the build status is, run these steps. There are other conditions
+        // available as well, such as "success", "failed", "unstable", and "changed".
+      //  always {
+        //    archive "target/**/*"
+         //   junit 'target/surefire-reports/*.xml'
+        //}
+    //}
+
+    stages {
+        // While there's only one stage here, you can specify as many stages as you like!
+        stage("build") {
+            steps {
+                echo "hello from stage-0"
+                workflowJob("MyFlow") 
+                //bat 'mvn clean install -Dmaven.test.failure.ignore=true'
+            }
+        }
+    }
+
+}
+'''
+)
+            sandbox()
+        }
+    }
+}
+        
+        
         pipeline {
     // Make sure that the tools we need are installed and on the path.
         tools {
