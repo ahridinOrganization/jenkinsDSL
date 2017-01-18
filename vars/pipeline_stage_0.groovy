27ed38766d00b482a7f6f906658da5de68fc7f85
ahridin <ahridin@gmail.com>
2017-01-18 03:14:00 +0200

    Update pipeline_stage_0.groovy

--- vars/pipeline_stage_0.groovy
+++ vars/pipeline_stage_0.groovy
@@ -1,3 +1,14 @@
+
+import com.cloudbees.groovy.cps.NonCPS
+
+@NonCPS
+def myMethod(String json) 
+def myJob = freeStyleJob('SimpleJob')
+myJob.with {
+    description 'A Simple Job'
+}
+
+
 def call(body) {
     def config = [:]
     def jobFolder="STAGE-0"
@@ -11,11 +22,8 @@ def call(body) {
         echo "Hello from pipeline_stage_0.groovy" 
         //load "${workspace}@libs/github.com/ahridinOrganization/jenkinsDSL/vars/pipeline0.groovy"           
         //pipelineScript(config)
-        def myJob = freeStyleJob('SimpleJob')
-        myJob.with {
-            description 'A Simple Job'
-        }
- //       workflowJob("testWworkflowJob")
+        run()
+        //       workflowJob("testWworkflowJob")
   //      buildFlowJob("${config.jobName}") {            
             tools {
                 maven "Maven 3.0.4"
