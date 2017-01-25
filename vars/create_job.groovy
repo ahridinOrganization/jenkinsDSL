ebdefa1e14fa4e6e38eb9d27b09a33d73b5f4004
ahridin <ahridin@gmail.com>
2017-01-25 11:14:09 +0200

    Update create_job.groovy

--- vars/create_job.groovy
+++ vars/create_job.groovy
@@ -6,8 +6,8 @@ def call(body) {
     body.delegate = config
     body()
     node {
-        echo config.JOB_SCRIPT
-        jobDsl ignoreMissingFiles: true, lookupStrategy: 'SEED_JOB', removedJobAction: 'DISABLE', removedViewAction: 'DELETE', targets: config.JOB_SCRIPT, unstableOnDeprecation: true        
+        echo "===> Job to be run = " + config.JOB_SCRIPT
+        jobDsl ignoreMissingFiles: true, lookupStrategy: 'SEED_JOB', removedJobAction: 'DISABLE', removedViewAction: 'DELETE', targets: "${config.JOB_SCRIPT}", unstableOnDeprecation: true        
     }
 }
 
