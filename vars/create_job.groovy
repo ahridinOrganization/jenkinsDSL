c339acbf896d8fe6c78128c4e97a8a9a58c2117c
ahridin <ahridin@gmail.com>
2017-01-25 11:33:33 +0200

    Update create_job.groovy

--- vars/create_job.groovy
+++ vars/create_job.groovy
@@ -9,21 +9,18 @@ def call(body) {
         //jobDsl ignoreMissingFiles: true, lookupStrategy: 'SEED_JOB', removedJobAction: 'DISABLE', removedViewAction: 'DELETE', targets: 'stage_0_pipeline.groovy', unstableOnDeprecation: true        
         jobDsl scriptText:"""
            folder("${jobFolder}")
-          
-
            pipelineJob(${jobFolder}/${config.NAME}") {
                 definition {
-                cps {
-                    scriptPath('/vars/stage_0_pipeline.groovy')
-                    sandbox()
+                    cps {
+                        scriptPath('/vars/stage_0_pipeline.groovy')
+                        sandbox()
                     }
                 }
-            }
-
-
            }
-       }
+        }
+    }
 }
+
 
 
 
