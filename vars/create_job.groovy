a347f0a03f2638442066ca9b151aa11b379a45a7
ahridin <ahridin@gmail.com>
2017-01-26 08:03:44 +0200

    Update create_job.groovy

--- vars/create_job.groovy
+++ vars/create_job.groovy
@@ -3,16 +3,16 @@ def call(body) {
     def jobFolder="STAGE-0"
     def job
     body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    LinkedHashMap<String, String> props = config    
-    def myJob = pipelineJob('${jobFolder}/${config.NAME}')
-    body()	        
+    body.delegate = config      
+    body()	 
+    def test="Anna " + config.MAVEN_GOALS
     node () {
+        dehco test
         echo config.MAVEN_GOALS 
         echo props.MAVEN_GOALS
-        jobDsl scriptText:"""folder("${jobFolder}")"""
-        //jobDsl ignoreMissingFiles: true, lookupStrategy: 'SEED_JOB', removedJobAction: 'DISABLE', removedViewAction: 'DELETE', targets: 'stage_0_pipeline.groovy', unstableOnDeprecation: true        
+       //jobDsl ignoreMissingFiles: true, lookupStrategy: 'SEED_JOB', removedJobAction: 'DISABLE', removedViewAction: 'DELETE', targets: 'stage_0_pipeline.groovy', unstableOnDeprecation: true        
         jobDsl scriptText:"""
+            folder("${jobFolder}")
             pipelineJob("${jobFolder}/${config.NAME}") {
                 definition {
                           cpsScm { scm {git('https://github.com/jenkinsci/job-dsl-plugin.git')}}
@@ -20,11 +20,10 @@ def call(body) {
                              choiceParam('choice', ['a', 'b', 'c'], 'FIXME')
                                      
 //predefinedProp('GIT_URL', "https://github.com/jenkinsci/job-dsl-plugin")                                
-  //                          stringParam('myParameterName', 'my default stringParam value')
+  //                          stringParam('myParameterName', test)
                              //predefinedProps(${props})
                              //predefinedProps([key2: 'value2', key3: 'value3'])
                           }                        
-                          publishers {aggregateDownstreamTestResults()}
                           cps {
                               script(readFileFromWorkspace("${config.SCRIPT}"))
                               sandbox()
