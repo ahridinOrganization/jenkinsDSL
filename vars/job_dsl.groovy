114444ebbc35918798946d9b579a1d03f2c9fb60
ahridin <ahridin@gmail.com>
2017-01-29 21:38:55 +0200

    Update job_dsl.groovy

--- vars/job_dsl.groovy
+++ vars/job_dsl.groovy
@@ -3,31 +3,32 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config      
     body()	 
-    def job  
-    def params="" 
     def jobFolder="STAGE-0"
-    for (item in config) {           
+    def job
+    def params=""
+    for (item in config) {
+        if (item != null)            
           params=params+"stringParam('${item.key.toString()}','${item.value.toString()}')\n"           
     }       
-    node () {    
-       println params
+    println params
+    node () {
        jobDsl ignoreMissingFiles: true, lookupStrategy: 'SEED_JOB', removedJobAction: 'DISABLE', removedViewAction: 'DELETE', scriptText:"""
             folder("${jobFolder}")
-            pipelineJob("${jobFolder}/${config.JOB}") {
-                  definition {
-                            cpsScm { scm {git('https://github.com/jenkinsci/job-dsl-plugin.git')}}
-                            parameters {
-                              ${params}
-                             booleanParam('myBool', false)                                                          
-                             //choiceParam('choice', ['a', 'b', 'c'], 'FIXME')
-                             //stringParam('myParameterName', ${test})                             
-                           }                        
-                           cps {
-                               script(readFileFromWorkspace(${config.SCRIPT}))
-                               sandbox()
-                           } //end cps
-                 } //end definition
-            } //end pipelinejob           
+            pipelineJob("${jobFolder}/${config.NAME}") {
+                definition {
+                          cpsScm { scm {git('https://github.com/jenkinsci/job-dsl-plugin.git')}}
+                          parameters {
+                             ${params}
+                            booleanParam('myBool', false)                                                          
+                            //choiceParam('choice', ['a', 'b', 'c'], 'FIXME')
+                            //stringParam('myParameterName', ${test})                             
+                          }                        
+                          cps {
+                              script(readFileFromWorkspace(${config.SCRIPT}))
+                              sandbox()
+                          } //end cps
+                } //end definition
+            } //end pipelinejob
         """ 
         job = build (job:"${jobFolder}/${config.NAME}")                     
     } //end node
