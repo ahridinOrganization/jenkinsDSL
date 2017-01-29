08af1c953c88ae1c989b678c6a2cc2e6fc5fc249
ahridin <ahridin@gmail.com>
2017-01-29 22:47:36 +0200

    Create create.groovy

---
+++ create.groovy
@@ -0,0 +1,35 @@
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config      
+    body()	 
+    def jobFolder="STAGE-0"
+    def job
+    def params=""
+    for (item in config) {
+        if (item != null)            
+          params=params+"stringParam('${item.key.toString()}','${item.value.toString()}')\n"           
+    }       
+    println params
+    node () {
+       jobDsl ignoreMissingFiles: true, lookupStrategy: 'SEED_JOB', removedJobAction: 'DISABLE', removedViewAction: 'DELETE', scriptText:"""
+            folder("${jobFolder}")
+            pipelineJob("${jobFolder}/${config.NAME}") {
+                definition {
+                          cpsScm { scm {git('https://github.com/jenkinsci/job-dsl-plugin.git')}}
+                          parameters {
+                             ${params}
+                            booleanParam('myBool', false)                                                          
+                            //choiceParam('choice', ['a', 'b', 'c'], 'FIXME')
+                            //stringParam('myParameterName', ${test})                             
+                          }                        
+                          cps {
+                              script(readFileFromWorkspace(${config.SCRIPT}))
+                              sandbox()
+                          } //end cps
+                } //end definition
+            } //end pipelinejob
+        """ 
+        job = build (job:"${jobFolder}/${config.NAME}")                     
+    } //end node
+} //end call
